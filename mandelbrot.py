b7768e1242feb26f10e48a67bf7fb56d1478b42b
Boris Kruglov <kruglov@restream.rt.ru>
2018-03-25 23:21:38 +0400

    use python's type for complex numbers

--- mandelbrot.py
+++ mandelbrot.py
@@ -4,29 +4,17 @@ def rpt(f, n, x):
     return rpt(f, n - 1, f(x))
 
 
-def mult(a, b, c, d):
-    return a * c - b * d, a * d + b * c
-
-
-def sum(a, b):
-    return a[0] + b[0], a[1] + b[1]
-
-
 def m(z, c):
-    return sum(mult(*z, *z), c)
+    return z *z + c
 
 
 def m_c(c):
     return lambda z: m(z, c)
 
 
-def scale(xs, ys, i, j):
+def scale_and_rotate(xs, ys, i, j):
     n = 50
-    return (j - ys) / n, 2 * (i - xs) / n,
-
-
-def sqrad(c):
-    return c[0] * c[0] + c[1] * c[1]
+    return complex((j - ys) / n, 2 * (i - xs) / n)
 
 
 xs = 80
@@ -35,7 +23,10 @@ di = 0
 colors = " .:-=+*#%@"
 for i in range(xs):
     for j in range(ys):
-        c = sqrad(rpt(m_c(scale(xs / 2, ys / 2, i - di, j)), 10, (0, 0)))
+        try:
+            c = abs(rpt(m_c(scale_and_rotate(xs / 2, ys / 2, i - di, j)), 10, complex(0, 0)))
+        except:
+            c=2
         s = colors[int(c * 10)] if c < 1.0 else " "
         print(s, end="")
     print()
